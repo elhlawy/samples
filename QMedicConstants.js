b84e1f2105d41874cffc9f3aef60fe5c17fd050b
acealbastoni <elhlawy@gmail.com>
2020-03-01 14:09:15 +0200

    Add files via upload

---
+++ QMedicConstants.js
@@ -0,0 +1,12 @@
+var QMedicConstants = {
+  AWS_KEY : 'AKIAJOV5K2BHGOVQNKWQQ',
+  AWS_SECRET : '6PdQTWMxz4noPXMi3y6yNqfceJ2KLw0EtUUL0x9ll',
+  QMedicRawData_FOLDER_ID : '0AHK-BiWqqAyEUk9PVAA',
+  TIME_IN_CACHE : 21600,
+  REGION : 'us-east-1',
+  BUCKET :  "qmedic-v4-productionn",
+  EXPIRES : 3600 * 24 * 7,
+  DayInMS: 3600000*24 ,  
+  SubscribersInfoSheetId:'1IdxgVcohmTu4HX0FwUc3pC-Kn_-ha5mtyTtuUPNJmFYY',
+  QMedicRawAnomally_FOLDER_ID : '0ABgD8PduJIf2Uk9PVAA'
+};
